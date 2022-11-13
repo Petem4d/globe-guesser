f6e1dfea91cb9ac0814b4a4069ce09759801ef43
Peter Ford <peter@54ds.net>
2022-11-13 11:41:06 -0500

    Loaded and added JSON file

---
+++ driver.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", setup); 
+
+var country = "";
+function setup(){
+    addListeners();
+    pickCountry();
+}
+
+function addListeners(){
+
+}
+
+function addHint(){
+
+}
+function pickCountry(){
+    
+}
---
+++ loadJSON.js
@@ -0,0 +1,9 @@
+import data from './Countries.json' assert { type: 'json' };
+window.addEventListener("load", main); 
+
+function main(){
+    console.log(data);
+}
+
+
+//    <script type="text/javascript" src="Countries.json"></script>
